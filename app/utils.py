cf377ec6b40c62daa69824d0b8fb23f83e9ef930
rimu <3310831+rimu@users.noreply.github.com>
2024-01-19 21:00:46 +1300

    allow spoiler html

--- app/utils.py
+++ app/utils.py
@@ -155,7 +155,7 @@ def allowlist_html(html: str) -> str:
     if html is None or html == '':
         return ''
     allowed_tags = ['p', 'strong', 'a', 'ul', 'ol', 'li', 'em', 'blockquote', 'cite', 'br', 'h3', 'h4', 'h5', 'pre',
-                    'code', 'img']
+                    'code', 'img', 'details', 'summary']
     # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser')
 
