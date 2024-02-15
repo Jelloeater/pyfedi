b864e223bc75e30beb63fc9983b35d66807207d0
rimu <3310831+rimu@users.noreply.github.com>
2024-02-16 06:34:56 +1300

    nsfw and nsfl icons on posts - oops

--- app/community/forms.py
+++ app/community/forms.py
@@ -103,7 +103,7 @@ class CreatePostForm(FlaskForm):
                     current_user.reputation -= 1
                     db.session.commit()
                     return False
-            community = Community.query.get(self.communities.data).first()
+            community = Community.query.get(self.communities.data)
             if community.is_local() and g.site.allow_local_image_posts is False:
                 self.communities.errors.append(_('Images cannot be posted to local communities.'))
         elif self.post_type.data == 'poll':
