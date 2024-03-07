3e5d040beb13c24b4e77b11ef0ad6791b6846931
freamon <f78673709@gmail.com>
2024-03-07 21:00:11 +0000

    Add header tags to allowlist

--- app/utils.py
+++ app/utils.py
@@ -163,7 +163,7 @@ def is_image_url(url):
 def allowlist_html(html: str) -> str:
     if html is None or html == '':
         return ''
-    allowed_tags = ['p', 'strong', 'a', 'ul', 'ol', 'li', 'em', 'blockquote', 'cite', 'br', 'h3', 'h4', 'h5', 'pre',
+    allowed_tags = ['p', 'strong', 'a', 'ul', 'ol', 'li', 'em', 'blockquote', 'cite', 'br', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'pre',
                     'code', 'img', 'details', 'summary', 'table', 'tr', 'td', 'th', 'tbody', 'thead']
     # Parse the HTML using BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser')
